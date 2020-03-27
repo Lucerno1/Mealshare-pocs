f33b66cdb981c093599fcb26a8ed3eb314f5588e
Lucerno1 <bram_vos@hotmail.com>
2020-03-27 15:33:17 +0100

    Added time picker

--- user-input-vuetify/src/components/MealForm.vue
+++ user-input-vuetify/src/components/MealForm.vue
@@ -1,16 +1,42 @@
 <template>
     <v-row align="center" justify="center" >
         <v-col cols="10">
-            <v-text-field color="green" class="mx-10 mt-10" label="Title" outlined v-model="title"></v-text-field>
-            <v-text-field color="green" class="mx-10" label="Price" outlined v-model="price"></v-text-field>
-            <v-text-field color="green" class="my-input mx-10" label="Timespan" outlined v-model="timespan"></v-text-field>
 
-            <!-- <input v-model="title" placeholder="Title"> 
-            <input v-model="price" placeholder="Price">
-            <input v-model="timespan" placeholder="Timespan"> -->
+            <h1>Share a meal</h1>
+            <!-- Text input fields -->
+            <v-text-field color="green" class="mt-10" label="Title" outlined v-model="title"></v-text-field>
+            
+            <v-row>
+                <v-col>
+                    <v-text-field color="green"  label="Price" outlined v-model="price"></v-text-field>
+                </v-col>
+                <v-col>
+                    <!-- Pop up dialog for timespan -->
+                    <v-dialog max-width="308px" v-model="dialog">
+                        <template v-slot:activator="{ on }">
+                            <v-btn height="56px" color="grey" outlined x-large v-on="on">Set Pickup Time</v-btn>
+                        </template>
 
+                        <v-card >
+                            <v-card-title class="headline grey lighten-2" primary-title>Set pickup time </v-card-title>
+                            <v-card-actions class="d-flex flex-column">
+                                <v-time-picker v-model="time"></v-time-picker>
+                                <p>{{time}}</p>
+
+                                <!-- Close the pop up dialog btn -->
+                                <v-btn width="100%" class="mt-3" color="primary" depressed x-large  @click="dialog = false">Apply</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+                </v-col>
+            </v-row>
+            
+                       
+            
+
+            <!-- Save to console log btn -->
             <v-row justify="center">
-                <v-btn class="white--text mx-10" depressed elevation="0" color="green" v-on:click="myFunction">Save to console</v-btn>
+                <v-btn width="100%" class="white--text ma-3" large depressed elevation="0" color="green" v-on:click="myFunction">Save to console</v-btn>
             </v-row>
         </v-col>
     </v-row>
@@ -23,13 +49,14 @@ export default {
     data(){
         return{
             title: "",
-            price: "",
-            timespan: ""
+            price: "",            
+            dialog: false,
+            time: ""
         }
     },
     methods: {
         myFunction:function(){
-            console.log({title:this.title, price:this.price, timespan:this.timespan})
+            console.log({title:this.title, price:this.price, pickuptime:this.time})
         }
     }
 }
